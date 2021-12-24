2430054cd21318c27dd769a4c7a12b3b8f285191
Sébastien Combéfis <sebastien@combefis.be>
2021-12-24 20:16:33 +0100

    Setup Apollo client

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,25 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <course-card :course="course" />
+      <course-card v-for="course in courses" :key="course.code" :course="course" />
     </v-col>
   </v-row>
 </template>
 
 <script>
+import { gql } from 'graphql-tag'
+
 export default {
-  name: 'IndexPage',
-  data() {
-    return {
-      course: {
-        code: 'SE410µ',
-        name: 'Gestion sécurisée des entrées des systèmes logiciels',
-        banner: 'https://www.tlca.eu/modules/courses/client/img/banner/uploads/5fc3b83e6c4fb97907544e98.jpg',
-        type: 'training'
+  apollo: {
+    courses: gql`query {
+      courses {
+        banner
+        code
+        name
+        type
       }
-    }
-  }
+    }`
+  },
+  name: 'IndexPage'
 }
 </script>
