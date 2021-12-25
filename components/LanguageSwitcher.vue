f735c6aec7120e0bc2ee223e596f3a176ff48e6e
Sébastien Combéfis <sebastien@combefis.be>
2021-12-25 16:11:17 +0100

    Fix language switcher actions

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-select v-model="currentLocale" :items="availableLocales" dense solo item-text="name" item-value="code">
+  <v-select v-model="currentLocale" :items="availableLocales" dense solo hide-details item-text="name" item-value="code">
     <template #selection="{ item }">
       <v-list-item dense>
         <v-list-item-content><v-list-item-title>{{ item.name }}</v-list-item-title></v-list-item-content>
       </v-list-item>
     </template>
-    <template #item="{ item }">
-      <v-list-item dense>
+    <template #item="{ item, on, attrs }">
+      <v-list-item dense v-bind="attrs" v-on="on">
         <v-list-item-avatar size="20"><v-img :src="`/flags/${item.code}.png`" /></v-list-item-avatar>
         <v-list-item-content><v-list-item-title>{{ item.name }}</v-list-item-title></v-list-item-content>
       </v-list-item>
