0b5f40d544efc5e5e95f2c5d17302aed794816ce
Sébastien Combéfis <sebastien@combefis.be>
2021-12-30 11:14:44 +0100

    Add spaces navigator drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,7 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      disable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    <space-navigator />
+    <v-app-bar clipped-left dense fixed app>
       <v-toolbar-title v-text="title" />
       <v-spacer />
     </v-app-bar>
@@ -34,7 +11,7 @@
       </v-container>
     </v-main>
     <v-footer absolute app>
-      <span>{{ $t('global.powered_by') }} AEI Consulting – v1.1.1.</span>
+      <span>{{ $t('global.powered_by') }} AEI Consulting – v1.2.0.</span>
       <v-spacer />
       <div>
         <language-switcher />
@@ -44,27 +21,16 @@
 </template>
 
 <script>
-import LanguageSwitcher from '~/components/LanguageSwitcher.vue'
+import LanguageSwitcher from '~/components/LanguageSwitcher.vue';
+import SpaceNavigator from '~/components/SpaceNavigator.vue';
+
 export default {
   name: 'DefaultLayout',
-  components: { LanguageSwitcher },
+  components: { LanguageSwitcher, SpaceNavigator },
   data() {
     return {
-      drawer: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      title: 'TLCA',
-    }
-  },
+      title: 'TLCA'
+    };
+  }
 }
 </script>
