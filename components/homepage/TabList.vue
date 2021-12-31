9b7e758fd25bec3dc2554a8dfdbf1e77768b4fe4
Sébastien Combéfis <sebastien@combefis.be>
2021-12-31 10:10:16 +0100

    Refactor courses guest pages

--- components/homepage/TabList.vue
+++ components/homepage/TabList.vue
@@ -1,30 +1,19 @@
 <template>
-  <div v-if="items">
-    <div v-if="items.length">
-      <v-row justify="start" align="stretch">
-        <v-col v-for="item in items" :key="item.code" cols="12" md="3" sm="4">
-          <component :is="component.name" v-bind="{ [component.propName]: item }" class="card" />
-        </v-col>
-      </v-row>
+  <card-list :name="name" :component="component" :items="items">
+    <template #append>
       <div align="center" class="py-8">
         <v-btn :to="{ name: name }">{{ $t(`homepage.see_all_${name}`) }}</v-btn>
       </div>
-    </div>
-    <div v-else align="center">{{ $t(`${name.slice(0, -1)}.no`) }}</div>
-  </div>
-  <!-- Boilerplate when items is not loaded yet -->
-  <div v-else>
-    <v-row justify="start" align="stretch">
-      <v-col v-for="i in 4" :key="i" cols="12" md="3" sm="4">
-        <v-skeleton-loader type="image"></v-skeleton-loader>
-      </v-col>
-    </v-row>
-  </div>
+    </template>
+  </card-list>
 </template>
 
 <script>
+import CardList from '~/components/cards/CardList.vue';
+
 export default {
   name: 'TabList',
+  components: { CardList },
   props: {
     component: {
       type: Object,
@@ -41,9 +30,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.card {
-  height: 100%;
-}
-</style>
