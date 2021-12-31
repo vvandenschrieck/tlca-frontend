cd471d33bf745307676007208a24b37ae3de53ec
Sébastien Combéfis <sebastien@combefis.be>
2021-12-31 10:25:17 +0100

    Add partners and programs guest pages

--- pages/partners/index.vue
+++ pages/partners/index.vue
@@ -3,42 +3,42 @@
     <v-card flat color="grey lighten-3" class="my-2">
       <v-breadcrumbs divider="/" :items="navItems" class="py-2"></v-breadcrumbs>
     </v-card>
-    <h2 v-t="{ path: 'course._', choice: 2 }" />
-    <card-list :name="panel.name" :component="panel.component" :items="courses" />
+    <h2 v-t="{ path: 'partner._', choice: 2 }" />
+    <card-list :name="panel.name" :component="panel.component" :items="partners" />
   </div>
 </template>
 
 <script>
 import { gql } from 'graphql-tag';
-import CourseCard from '~/components/cards/CourseCard.vue';
+import PartnerCard from '~/components/cards/PartnerCard.vue';
 import CardList from '~/components/cards/CardList.vue';
 
 export default {
-  name: 'CoursesPage',
+  name: 'PartnersPage',
   components: { CardList },
   data() {
     return {
       navItems: [{
-        text: this.$tc('course._', 2),
+        text: this.$tc('partner._', 2),
         exact: true,
-        to: { name: 'courses' }
+        to: { name: 'partners' }
       }],
       panel: {
-        name: 'courses',
+        name: 'partners',
         component: {
-          name: CourseCard,
-          propName: 'course'
+          name: PartnerCard,
+          propName: 'partner'
         }
       }
     };
   },
   apollo: {
-    courses: gql`query {
-      courses {
+    partners: gql`query {
+      partners {
+        abbreviation
         banner
         code
         name
-        type
       }
     }`
   }
