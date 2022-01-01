50345b2c7d847837b3bb86939a921a387d0b7d2a
Sébastien Combéfis <sebastien@combefis.be>
2022-01-01 18:56:20 +0100

    Initialise manage, teach and learn spaces

---
+++ pages/learn/courses.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>Learn space : courses</p>
+</template>
+
+<script>
+export default {
+  name: 'LearnCoursesPage'
+}
+</script>
---
+++ pages/learn/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>Learn space</p>
+</template>
+
+<script>
+export default {
+  name: 'LearnPage'
+}
+</script>
