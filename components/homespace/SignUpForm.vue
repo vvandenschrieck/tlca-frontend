e1b4444a5fd785ccc0c1152ad40ba35d502a4d02
Sébastien Combéfis <sebastien@combefis.be>
2022-03-20 18:32:30 +0100

    Add sign up form and improve sign in
    - Added sign up feature
    - Improved sign in form error messages
    - Added cleaner management of GraphQL errors

--- components/homespace/SignUpForm.vue
+++ components/homespace/SignUpForm.vue
@@ -1,22 +1,19 @@
 <template>
   <v-dialog v-model="dialog" :persistent="formBusy" max-width="500" :fullscreen="$vuetify.breakpoint.xsOnly">
     <template #activator="{ on, attrs }">
-      <v-tooltip bottom open-delay="1000">
-        <template #activator="{ on:tooltip }">
-          <v-btn v-bind="attrs" icon v-on="{ ...on, ...tooltip }">
-            <v-icon>mdi-login</v-icon>
-          </v-btn>
-        </template>
-        <span>{{ $t('authentication.sign_in') }}</span>
-      </v-tooltip>
+      <v-btn v-bind="attrs" text small v-on="{ ...on }">
+        {{ $t('authentication.sign_up') }}
+      </v-btn>
     </template>
     <v-card>
-      <v-card-title class="text-h5 grey lighten-2">{{ $t('authentication.sign_in') }}</v-card-title>
+      <v-card-title class="text-h5 grey lighten-2">{{ $t('authentication.sign_up') }}</v-card-title>
       <v-card-text>
         <v-alert v-if="error" type="error" outlined dense class="mt-5">
           {{ $t(error) }}
         </v-alert>
         <v-form ref="form" class="mt-5">
+          <v-text-field v-model="firstName" :label="$t('user.first_name')" required></v-text-field>
+          <v-text-field v-model="lastName" :label="$t('user.last_name')" required></v-text-field>
           <v-text-field v-model="email" :label="$t('user.email')" required></v-text-field>
           <v-text-field v-model="password" type="password" :label="$t('user.password')" required></v-text-field>
         </v-form>
@@ -24,21 +21,25 @@
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn :disabled="formBusy" color="error" text @click="cancel()">{{ $t('general.cancel') }}</v-btn>
-        <v-btn :loading="formBusy" color="primary" text @click="signIn()">{{ $t('authentication.sign_in') }}</v-btn>
+        <v-btn :loading="formBusy" color="primary" text @click="signUp()">{{ $t('authentication.sign_up') }}</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
+import signUpGql from '~/gql/signUp.gql';
+
 export default {
-  name: 'SignInForm',
+  name: 'SignUpForm',
   data() {
     return {
       dialog: false,
       email: '',
       error: null,
+      firstName: '',
       formBusy: false,
+      lastName: '',
       password: ''
     };
   },
@@ -52,20 +53,31 @@ export default {
   methods: {
     cancel() {
       this.email = '';
+      this.firstName = '';
+      this.lastName = '';
       this.password = '';
       this.error = null;
       this.dialog = false;
     },
-    async signIn() {
+    async signUp() {
       this.formBusy = true;
 
       try {
-        const credentials = { email: this.email, password: this.password };
-        await this.$auth.loginWith('graphql', credentials);
-        this.dialog = false;
+        const data = { firstName: this.firstName, lastName: this.lastName, email: this.email, password: this.password };
+
+        const response = await this.$apollo.mutate({
+          mutation: signUpGql,
+          variables: data
+        }).then(({ data }) => data && data.signUp);
+
+        if (response) {
+          this.dialog = false;
+        } else {
+          this.error = 'error._';
+        }
       } catch (err) {
         if (err.graphQLErrors?.length) {
-          this.error = `error.${err.graphQLErrors[0].message}`;
+          this.error = `error.${err.graphQLErrors[0].message}`
         } else {
           this.error = 'error._';
         }
