c7d897b6a2d3dcd6ff1b15961953b2f6b0a5f9ee
Sébastien Combéfis <sebastien@combefis.be>
2022-04-03 10:45:27 +0200

    Refactor partner detail page

--- pages/partners/_code.vue
+++ pages/partners/_code.vue
@@ -1,36 +1,31 @@
 <template>
   <ApolloQuery
     :query="require('../../gql/getPartner.gql')"
+    :update="updatePartner"
     :variables="{ code: $route.params.code }"
   >
-    <template #default="{ result: { loading, error, data } }">
-      <div v-if="loading">Loading...</div>
+    <template #default="{ result: { error, data: partner }, isLoading }">
+      <div v-if="isLoading" v-t="'global.loading'"></div>
 
-      <div v-else-if="error">An error occurred</div>
+      <div v-else-if="partner">
+        <bread-crumb :items="homespaceNavItems('partner', partner.abbreviation || partner.name)" />
 
-      <div v-else-if="data">
-        <v-card flat color="grey lighten-3" class="my-2">
-          <v-breadcrumbs
-            divider="/"
-            :items="navItems(data.partner)"
-            class="py-2"
-          ></v-breadcrumbs>
-        </v-card>
-
-        <h2>{{ data.partner.name }}</h2>
+        <h2>{{ partner.name }}</h2>
 
         <v-row>
           <v-col cols="12" md="9">
             <div>
-              <div v-html="data.partner.description"></div>
+              <div v-html="partner.description"></div>
             </div>
+
             <h3>{{ $tc('course._', 2) }}</h3>
+
             <card-list
-              :name="name"
-              :component="component"
-              :items="data.partner.courses || []"
-              :items-per-page="8"
               :cards-per-page="3"
+              :component="component"
+              :items="partner.courses || []"
+              :items-per-page="8"
+              :prop-name="propName"
             />
           </v-col>
           <v-col
@@ -41,71 +36,48 @@
             <info-panel
               :title="$t('global.information')"
               icon="mdi-information"
-              :items="infoItems(data.partner)"
+              :items="partner.infoItems"
             />
             <v-card flat class="mt-6 px-2">
               <v-img
-                v-if="data.partner.logo"
-                :src="data.partner.logo"
+                v-if="partner.logo"
+                :src="partner.logo"
                 contain
               />
             </v-card>
           </v-col>
         </v-row>
       </div>
+
+      <div v-else-if="error">An error occurred</div>
     </template>
   </ApolloQuery>
 </template>
 
 <script>
 import CourseCard from '~/components/cards/CourseCard.vue'
+import breadcrumb from '@/mixins/breadcrumb.js'
+import infopanel from '@/mixins/infopanel.js'
 
 export default {
   name: 'PartnerPage',
+  mixins: [breadcrumb, infopanel],
   data() {
     return {
-      name: 'courses',
-      component: {
-        name: CourseCard,
-        propName: 'course',
+      component: CourseCard,
+      infoItemFields: {
+        website: 'mdi-web',
+        abbreviation: 'mdi-domain',
+        tags: 'mdi-tag-multiple',
       },
+      propName: 'course'
     }
   },
   methods: {
-    infoItems(partner) {
-      const items = []
-      if (partner.website) {
-        items.push({
-          icon: 'mdi-web',
-          text: partner.website,
-          tooltip: this.$t('partner.website'),
-        })
-      }
-      if (partner.abbreviation) {
-        items.push({
-          icon: 'mdi-domain',
-          text: partner.abbreviation,
-          tooltip: this.$t('partner.abbreviation'),
-        })
-      }
-      return items
-    },
-    navItems(partner) {
-      return [
-        {
-          text: this.$tc('global.spaces.home'),
-          exact: true,
-          to: { name: 'index' },
-        },
-        {
-          text: this.$tc('partner._', 2),
-          exact: true,
-          to: { name: 'partners' },
-        },
-        {
-          text: partner.abbreviation ?? partner.name,
-        },
-      ]
+    updatePartner(data) {
+      const partner = data.partner
+      partner.infoItems = this.infoItems('partner', partner, this.infoItemFields)
+      return partner
     },
   },
 }
