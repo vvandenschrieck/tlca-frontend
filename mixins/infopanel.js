2ad8ef5b9e1d6d9a552752673f79c293601f8b63
Sébastien Combéfis <sebastien@combefis.be>
2022-04-03 10:31:50 +0200

    Refactor course detail page

---
+++ mixins/infopanel.js
@@ -0,0 +1,18 @@
+export default {
+  methods: {
+    infoItems(section, source, fields) {
+      const items = []
+      for (const [field, icon] of Object.entries(fields)) {
+        const value = source[field]
+        if (value) {
+          items.push({
+            icon,
+            text: Array.isArray(value) ? value.join(', ') : value,
+            tooltip: this.$t(`${section}.${field}`),
+          })
+        }
+      }
+      return items
+    },
+  },
+}
--- pages/courses/_code.vue
+++ pages/courses/_code.vue
@@ -1,133 +1,110 @@
 <template>
-  <div v-if="course">
-    <v-card flat color="grey lighten-3" class="my-2">
-      <v-breadcrumbs divider="/" :items="navItems" class="py-2"></v-breadcrumbs>
-    </v-card>
-    <h2>{{ course.name }}</h2>
-    <v-row>
-      <v-col cols="12" md="9">
-        <v-card>
-        <v-tabs v-model="currentTab" show-arrows>
-          <v-tab>{{ $t('course.description') }}</v-tab>
-          <v-tab>{{ $t('course.competencies') }}</v-tab>
-          <v-tab v-if="course.schedule">{{ $t('course.schedule._') }}</v-tab>
-          <v-tab v-if="course.colophon">{{ $t('course.colophon') }}</v-tab>
-        </v-tabs>
-        <v-card-text class="text--primary">
-          <v-tabs-items v-model="currentTab">
-            <v-tab-item>
-              <div v-html="course.description"></div>
-            </v-tab-item>
-            <v-tab-item>
-              <competency-list :items="course.competencies" />
-            </v-tab-item>
-            <v-tab-item v-if="course.schedule">
-              <v-timeline>
-                <v-timeline-item v-for="{ name, date } in schedule" :key="name" small right :color="isInPast(date) ? 'grey' : undefined">
-                  <span slot="opposite">{{ $t(`course.schedule.${name}`) }}</span>
-                  {{ formatDateTimeFull(date) }}
-                </v-timeline-item>
-              </v-timeline>
-            </v-tab-item>
-            <v-tab-item v-if="course.colophon">
-              <div v-html="course.colophon"></div>
-            </v-tab-item>
-          </v-tabs-items>
-        </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="3" :order="$vuetify.breakpoint.smAndDown ? 'first' : undefined">
-        <info-panel :title="$t('global.information')" icon="mdi-information" :items="infoItems" />
-      </v-col>
-    </v-row>
-  </div>
+  <ApolloQuery
+    :query="require('../../gql/getCourse.gql')"
+    :update="updateCourse"
+    :variables="{ code: $route.params.code }"
+  >
+    <template #default="{ result: { error, data: course }, isLoading }">
+      <div v-if="isLoading" v-t="'global.loading'"></div>
+
+      <div v-else-if="course">
+        <bread-crumb :items="homespaceNavItems('course', $route.params.code)" />
+
+        <h2>{{ course.name }}</h2>
+
+        <v-row>
+          <v-col cols="12" md="9">
+            <v-card>
+              <v-tabs v-model="currentTab" show-arrows>
+                <v-tab v-t="'course.description'"></v-tab>
+                <v-tab v-t="'course.competencies'"></v-tab>
+                <v-tab v-if="course.schedule" v-t="'course.schedule._'"></v-tab>
+                <v-tab v-if="course.colophon" v-t="'course.colophon'"></v-tab>
+              </v-tabs>
+              <v-card-text class="text--primary">
+                <v-tabs-items v-model="currentTab">
+                  <v-tab-item>
+                    <div v-html="course.description"></div>
+                  </v-tab-item>
+
+                  <v-tab-item>
+                    <competency-list :items="course.competencies" />
+                  </v-tab-item>
+
+                  <v-tab-item v-if="course.schedule">
+                    <v-timeline>
+                      <v-timeline-item
+                        v-for="{ name, date } in course.schedule"
+                        :key="name"
+                        small
+                        right
+                        :color="isInPast(date) ? 'grey' : undefined"
+                      >
+                        <span slot="opposite" v-t="`course.schedule.${name}`"></span>
+                        {{ formatDateTimeFull(date) }}
+                      </v-timeline-item>
+                    </v-timeline>
+                  </v-tab-item>
+
+                  <v-tab-item v-if="course.colophon">
+                    <div v-html="course.colophon"></div>
+                  </v-tab-item>
+                </v-tabs-items>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col
+            cols="12"
+            md="3"
+            :order="$vuetify.breakpoint.smAndDown ? 'first' : undefined"
+          >
+            <info-panel
+              :title="$t('global.information')"
+              icon="mdi-information"
+              :items="course.infoItems"
+            />
+          </v-col>
+        </v-row>
+      </div>
+
+      <div v-else-if="error">An error occurred</div>
+    </template>
+  </ApolloQuery>
 </template>
 
 <script>
-import { gql } from 'graphql-tag';
-import { DateTime } from 'luxon';
-import datetime from '@/mixins/datetime.js';
+import { DateTime } from 'luxon'
+import breadcrumb from '@/mixins/breadcrumb.js'
+import datetime from '@/mixins/datetime.js'
+import infopanel from '@/mixins/infopanel.js'
 
 export default {
   name: 'CoursePage',
-  mixins: [
-    datetime
-  ],
+  mixins: [breadcrumb, datetime, infopanel],
   data() {
     return {
-      currentTab: 'competencies'
-    };
+      currentTab: 'competencies',
+      infoItemFields: {
+        field: 'mdi-school',
+        language: 'mdi-message',
+        tags: 'mdi-tag-multiple',
+      },
+    }
   },
-  computed: {
-    infoItems() {
-      const items = [];
-      if (this.course.field) {
-        items.push({
-          icon: 'mdi-school',
-          text: this.course.field,
-          tooltip: this.$t('course.field')
-        });
-      }
-      if (this.course.language) {
-        items.push({
-          icon: 'mdi-message',
-          text: this.course.language,
-          tooltip: this.$t('course.language')
-        });
-      }
-      return items;
-    },
-    navItems() {
-      return [{
-        text: this.$tc('global.spaces.home'),
-        exact: true,
-        to: { name: 'index' }
-      }, {
-        text: this.$tc('course._', 2),
-        exact: true,
-        to: { name: 'courses' }
-      }, {
-        text: this.$route.params.code
-      }];
-    },
-    schedule() {
-      return this.course.schedule
+  methods: {
+    schedule(course) {
+      return course.schedule
         .map(({ name, date }) => ({ name, date: DateTime.fromISO(date) }))
-        .sort((a, b) => a.date - b.date);
-    }
-  },
-  apollo: {
-    course: {
-      query: gql`query Course($code: String!) {
-        course(code: $code) {
-          code
-          colophon
-          competencies {
-            competency {
-              code
-              description
-              name
-            }
-            category
-            subcategory
-          }
-          description
-          field
-          language
-          name
-          schedule {
-            name
-            date
-          }
-          type
-        }
-      }`,
-      variables() {
-        return {
-          code: this.$route.params.code
-        };
+        .sort((a, b) => a.date - b.date)
+    },
+    updateCourse(data) {
+      const course = data.course
+      course.infoItems = this.infoItems('course', course, this.infoItemFields)
+      if (course.schedule) {
+        course.schedule = this.schedule(course)
       }
-    }
-  }
+      return course
+    },
+  },
 }
 </script>
