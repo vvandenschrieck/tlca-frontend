043f45e1b68ffe8027ac03996cc355d569bc201f
Sébastien Combéfis <sebastien@combefis.be>
2022-06-25 22:34:07 +0200

    Update the sign up form
    - Removed first and last names from the signing up form
    - Updated the call to the GraphQL mutation

--- components/homespace/SignUpForm.vue
+++ components/homespace/SignUpForm.vue
@@ -1,34 +1,70 @@
 <template>
-  <v-dialog v-model="dialog" :persistent="formBusy" max-width="500" :fullscreen="$vuetify.breakpoint.xsOnly">
+  <v-dialog
+    v-model="dialog"
+    :persistent="formBusy"
+    max-width="500"
+    :fullscreen="$vuetify.breakpoint.xsOnly"
+  >
     <template #activator="{ on, attrs }">
-      <v-btn v-bind="attrs" text small v-on="{ ...on }">
-        {{ $t('authentication.sign_up') }}
-      </v-btn>
+      <v-btn
+        v-t="'authentication.sign_up'"
+        v-bind="attrs"
+        text
+        small
+        v-on="{ ...on }"
+      ></v-btn>
     </template>
     <v-card>
-      <v-card-title class="text-h5 grey lighten-2">{{ $t('authentication.sign_up') }}</v-card-title>
+      <v-card-title
+        v-t="'authentication.sign_up'"
+        class="text-h5 grey lighten-2"
+      ></v-card-title>
       <v-card-text>
-        <v-alert v-if="error" type="error" outlined dense class="mt-5">
-          {{ $t(error) }}
-        </v-alert>
+        <v-alert
+          v-if="error"
+          v-t="error"
+          type="error"
+          outlined
+          dense
+          class="mt-5"
+        ></v-alert>
         <v-form ref="form" class="mt-5">
-          <v-text-field v-model="firstName" :label="$t('user.first_name')" required></v-text-field>
-          <v-text-field v-model="lastName" :label="$t('user.last_name')" required></v-text-field>
-          <v-text-field v-model="email" :label="$t('user.email')" required></v-text-field>
-          <v-text-field v-model="password" type="password" :label="$t('user.password')" required></v-text-field>
+          <v-text-field
+            v-model="email"
+            :label="$t('user.email')"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            type="password"
+            :label="$t('user.password')"
+            required
+          ></v-text-field>
         </v-form>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn :disabled="formBusy" color="error" text @click="cancel()">{{ $t('general.cancel') }}</v-btn>
-        <v-btn :loading="formBusy" color="primary" text @click="signUp()">{{ $t('authentication.sign_up') }}</v-btn>
+        <v-btn
+          v-t="'general.cancel'"
+          :disabled="formBusy"
+          color="error"
+          text
+          @click="cancel()"
+        ></v-btn>
+        <v-btn
+          v-t="'authentication.sign_up'"
+          :loading="formBusy"
+          color="primary"
+          text
+          @click="signUp()"
+        ></v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
-import signUpGql from '~/gql/signUp.gql';
+import signUpGql from '~/gql/signUp.gql'
 
 export default {
   name: 'SignUpForm',
@@ -37,53 +73,54 @@ export default {
       dialog: false,
       email: '',
       error: null,
-      firstName: '',
       formBusy: false,
-      lastName: '',
-      password: ''
-    };
+      password: '',
+    }
   },
   watch: {
     dialog(value) {
       if (!value) {
-        this.cancel();
+        this.cancel()
       }
-    }
+    },
   },
   methods: {
     cancel() {
-      this.email = '';
-      this.firstName = '';
-      this.lastName = '';
-      this.password = '';
-      this.error = null;
-      this.dialog = false;
+      this.dialog = false
+      this.email = ''
+      this.error = null
+      this.password = ''
     },
     async signUp() {
-      this.formBusy = true;
+      this.formBusy = true
 
       try {
-        const data = { firstName: this.firstName, lastName: this.lastName, email: this.email, password: this.password };
+        const data = {
+          email: this.email,
+          password: this.password,
+        }
 
-        const response = await this.$apollo.mutate({
-          mutation: signUpGql,
-          variables: data
-        }).then(({ data }) => data && data.signUp);
+        const response = await this.$apollo
+          .mutate({
+            mutation: signUpGql,
+            variables: data,
+          })
+          .then(({ data }) => data && data.signUp)
 
         if (response) {
-          this.dialog = false;
+          this.dialog = false
         } else {
-          this.error = 'error._';
+          this.error = 'error._'
         }
       } catch (err) {
         if (err.graphQLErrors?.length) {
           this.error = `error.${err.graphQLErrors[0].message}`
         } else {
-          this.error = 'error._';
+          this.error = 'error._'
         }
       }
-      this.formBusy = false;
-    }
-  }
+      this.formBusy = false
+    },
+  },
 }
 </script>
