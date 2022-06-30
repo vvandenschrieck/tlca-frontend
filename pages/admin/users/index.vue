edcad3c44cb21dd98eb1a3c5dfffc5b85eb3ae10
Sébastien Combéfis <sebastien@combefis.be>
2022-06-30 22:24:54 +0200

    Add users page for admin
    - Created the users page in admin section
    - Added a GraphQL query to retrieve users

--- pages/admin/users/index.vue
+++ pages/admin/users/index.vue
@@ -7,10 +7,43 @@
       <div v-if="isLoading || data">
         <bread-crumb :items="navItems('admin', 'user')" />
 
-        <p>Admin space : users</p>
-        <ul>
-          <li v-for="user in data" :key="user.id">{{ user }}</li>
-        </ul>
+        <h2>{{ $tc('user._', 2) }}</h2>
+
+        <v-data-table
+          v-if="data"
+          :headers="headers"
+          :items="data"
+          :items-per-page="5"
+          class="elevation-1"
+        >
+          <template #item.firstName="{ value: firstName }">{{ firstName || '—' }}</template>
+          <template #item.lastName="{ value: lastName }">{{ lastName || '—' }}</template>
+          <template #item.roles="{ value: roles }">
+            <v-chip-group active-class="primary--text" column max="0" multiple>
+              <v-chip
+                v-for="role in roles"
+                :key="role"
+                v-t="`user.role.${role}`"
+                small
+              >
+              </v-chip>
+            </v-chip-group>
+          </template>
+          <template #item.isValidated="{ value: validated }">
+            <div class="d-inline-block">
+              <span
+                class="rounded-circle pa-1 d-flex"
+                :class="{ error: !validated, success: validated }"
+              >
+                <v-icon small color="white">{{
+                  validated ? 'mdi-check-bold' : 'mdi-close-thick'
+                }}</v-icon>
+              </span>
+            </div>
+          </template>
+        </v-data-table>
+
+        <v-skeleton-loader v-else type="table"></v-skeleton-loader>
       </div>
 
       <div v-else-if="error">An error occurred</div>
@@ -24,6 +57,21 @@ import breadcrumb from '@/mixins/breadcrumb.js'
 export default {
   name: 'AdminUsersPage',
   mixins: [breadcrumb],
+  computed: {
+    headers() {
+      return [
+        { text: this.$t('user.first_name'), value: 'firstName' },
+        { text: this.$t('user.last_name'), value: 'lastName' },
+        { text: this.$t('user.username'), value: 'username' },
+        { text: this.$tc('user.role._', 2), value: 'roles' },
+        {
+          text: this.$t('general.validated'),
+          value: 'isValidated',
+          align: 'center',
+        },
+      ]
+    },
+  },
   meta: {
     roles: ['admin'],
   },
