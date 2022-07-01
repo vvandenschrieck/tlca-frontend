60162a79e0d064f538ab2145ed01395583d8f677
Sébastien Combéfis <sebastien@combefis.be>
2022-07-01 17:18:38 +0200

    Add competency creation button for teachers
    - Added an information panel with the total number of competencies
    - Added an empty competency creation page
    - Added a competency creation button on the competencies list page

---
+++ pages/manage/competencies/create.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <bread-crumb
+      :items="navItems('manage', 'competency', $t('competency.create'))"
+    />
+
+    <p>Create competency</p>
+  </div>
+</template>
+
+<script>
+import breadcrumb from '@/mixins/breadcrumb.js'
+
+export default {
+  name: 'ManageCreateCompetencyPage',
+  mixins: [breadcrumb],
+  meta: {
+    roles: ['teacher'],
+  },
+}
+</script>
