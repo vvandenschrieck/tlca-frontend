90500893d486a189674da309e8cdfc7403482795
Sébastien Combéfis <sebastien@combefis.be>
2022-07-08 09:25:06 +0200

    Add assessments list and detail for course in manage space
    - Added assessments list and assessment detail pages
    - Created GraphQL queries to retrieve assessments list

---
+++ pages/manage/courses/_code/assessments/_id.vue
@@ -0,0 +1,44 @@
+<template>
+  <ApolloQuery
+    :query="require('~/gql/manage/getCourseAssessment.gql')"
+    :variables="{ courseCode: $route.params.code, assessmentId: $route.params.id }"
+  >
+    <template #default="{ result: { error, data }, isLoading }">
+      <div v-if="isLoading" v-t="'global.loading'"></div>
+
+      <div v-else-if="data.course && data.assessment">
+        <bread-crumb :items="navItems('manage', 'course', data.course.code)" />
+
+        <h2 v-text="data.assessment.name"></h2>
+
+        <v-row>
+          <v-col cols="12" md="9">
+            <p>TODO: Assessment detail</p>
+            <p>{{ data.assessment }}</p>
+          </v-col>
+          <v-col
+            cols="12"
+            md="3"
+            :order="$vuetify.breakpoint.smAndDown ? 'first' : undefined"
+          >
+            <p>TODO</p>
+          </v-col>
+        </v-row>
+      </div>
+
+      <div v-else-if="error">An error occurred</div>
+    </template>
+  </ApolloQuery>
+</template>
+
+<script>
+import breadcrumb from '@/mixins/breadcrumb.js'
+
+export default {
+  name: 'ManageCourseAssessmentPage',
+  mixins: [breadcrumb],
+  meta: {
+    roles: ['teacher'],
+  },
+}
+</script>
