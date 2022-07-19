3a2a14b585a1b2a07a07d1a398d1b2f3f52f859e
Sébastien Combéfis <sebastien@combefis.be>
2022-07-19 17:13:46 +0200

    Add filter bar to the programs list page in manage space
    - Created a program bar and program filter components
    - Added the program bar on the programs list page in manage space

---
+++ pages/manage/programs/create.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>Manage : programs : create</p>
+</template>
+
+<script>
+export default {
+  name: 'ManageCreateProgramPage',
+  meta: {
+    roles: ['teacher'],
+  },
+}
+</script>
