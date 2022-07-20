bb9947bbd8107e4779900ca96b7c085e97a65bc6
Virginie Van den Schrieck <v.vandenschrieck@gmail.com>
2022-07-20 16:07:49 +0200

    Manage : List of assessments

---
+++ components/infopanels/AssessmentsListInfoPanel.vue
@@ -0,0 +1,42 @@
+<template>
+  <generic-info-panel
+    :title="$t('general.information._')"
+    icon="mdi-information"
+    :items="items"
+  >
+    <div class="text-center">
+      <v-btn small color="success" :to="{ name: 'manage-courses-code-assessment-create' }">
+        <v-icon left>mdi-plus</v-icon>
+        <span v-t="'general.create'"></span>
+      </v-btn>
+    </div>
+  </generic-info-panel>
+</template>
+
+<script>
+export default {
+  name: 'AssessmentsListInfoPanel',
+  props: {
+    assessments: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    items() {
+      const items = []
+
+      // Number of assessments
+      items.push({
+        icon: 'mdi-clipboard-text',
+        text: this.$tc('assessment.nb', this.assessments.length, {
+          nb: this.assessments.length,
+        }),
+        tooltip: this.$t('assessment.nb_of'),
+      })
+
+      return items
+    },
+  },
+}
+</script>
