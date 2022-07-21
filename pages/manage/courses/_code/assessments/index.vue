12cd4c6fe1e69a5f2541a6029f54331501faf723
Sébastien Combéfis <sebastien@combefis.be>
2022-07-21 08:17:03 +0200

    Fix formatting issues and code style

--- pages/manage/courses/_code/assessments/index.vue
+++ pages/manage/courses/_code/assessments/index.vue
@@ -5,41 +5,48 @@
     :variables="{ code: $route.params.code }"
   >
     <template #default="{ result: { error, data: course }, isLoading }">
-      <div v-if="isLoading" v-t="'global.loading'"></div>
+      <div v-if="isLoading">{{ $t('global.loading') }}</div>
 
       <div v-else-if="course">
-        <h2 v-text="course.name"></h2>
+        <h2>{{ course.name }}</h2>
+
         <v-row>
-          <v-col cols="12" md="8">
+          <v-col cols="12" md="9">
             <v-card v-if="course.assessments && course.assessments.length">
               <v-list class="pa-0">
                 <template v-for="(assessment, i) in course.assessments">
                   <v-list-item
                     :key="assessment.code"
-                     :to="{
+                    :to="{
                       name: 'manage-courses-code-assessments-id',
                       params: { code: course.code, id: assessment.id },
                     }"
                   >
                     <v-list-item-content>
                       <v-list-item-title>
-                        <b>{{ assessment.code }}</b> – {{ assessment.name }}
+                        <b>{{ assessment.code }}</b>
+                        &nbsp;–&nbsp;{{ assessment.name }}
                       </v-list-item-title>
                       <v-list-item-subtitle>
                         <!-- $t('assessment.category') : {{ assessment.category }} -->
                       </v-list-item-subtitle>
                     </v-list-item-content>
                   </v-list-item>
-                  <v-divider v-if="i < course.assessments.length - 1" :key="i"></v-divider>
+                  <v-divider
+                    v-if="i < course.assessments.length - 1"
+                    :key="i"
+                  />
                 </template>
               </v-list>
             </v-card>
 
-            <div
-              v-else-if="course.assessments && !course.assessments.length"
-              v-t="'assessment.no'"
-            ></div>
+            <div v-else-if="course.assessments && !course.assessments.length">
+              {{ $('assessment.no') }}
+            </div>
+
+            <v-skeleton-loader v-else type="table" />
           </v-col>
+
           <v-col
             cols="12"
             md="3"
@@ -54,7 +61,7 @@
         </v-row>
       </div>
 
-      <div v-else-if="error">An error occurred</div>
+      <div v-else-if="error">{{ $t('error.unexpected') }}</div>
     </template>
   </ApolloQuery>
 </template>
