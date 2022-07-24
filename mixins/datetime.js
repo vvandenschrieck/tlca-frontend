a13a2e02a23d02df60028f938eb4c47089221c61
Sébastien Combéfis <sebastien@combefis.be>
2022-07-24 15:54:27 +0200

    Create assessment creation page
    - Created components to select a datetime, to select a number of stars
    and to select competencies for an assessment
    - Created the assessment creation form page
    - Defined GraphQL query to retrieve course competencies
    - Defined GraphQL mutation to create an assessment

--- mixins/datetime.js
+++ mixins/datetime.js
@@ -7,9 +7,18 @@ export default {
     },
     formatDateTime(datetime, format) {
       if (datetime instanceof String || typeof datetime === 'string') {
-        datetime = DateTime.fromISO(datetime)
+        if (datetime.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}$/g)) {
+          datetime = DateTime.fromFormat(datetime, 'yyyy-LL-dd hh:mm')
+        } else {
+          datetime = DateTime.fromISO(datetime)
+        }
+
+        if (!datetime.invalid) {
+          return datetime.setLocale(this.$i18n.locale).toLocaleString(format)
+        }
       }
-      return datetime.setLocale(this.$i18n.locale).toLocaleString(format)
+
+      return ''
     },
     formatDateFull(datetime) {
       return this.formatDateTime(datetime, DateTime.DATE_MED_WITH_WEEKDAY)
--- plugins/validation.js
+++ plugins/validation.js
@@ -1,14 +1,21 @@
 /* eslint-disable camelcase */
 
 import { extend } from 'vee-validate'
-import { alpha_dash, required } from 'vee-validate/dist/rules'
-
-extend('required', {
-  ...required,
-  message: 'This field is required',
-})
+import { alpha_dash, between, required } from 'vee-validate/dist/rules'
 
 extend('alpha_dash', {
   ...alpha_dash,
   message: 'This field can only contain characters and dashes',
+})
+
+extend('stars_required', {
+  validate(value) {
+    return between.validate(value, { min: 1, max: 3 })
+  },
+  message: 'This field is required',
+})
+
+extend('required', {
+  ...required,
+  message: 'This field is required',
 })
