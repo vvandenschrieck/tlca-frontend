8d1887c24f483ed0db98e75939ca659244eaf403
Sébastien Combéfis <sebastien@combefis.be>
2022-07-29 09:11:34 +0200

    Add publish and archive actions for courses in manage space
    - Added publish and archive buttons in the course status info panel
    - Created GraphQL mutations to archive and publish course

--- components/infopanels/CourseStatusInfoPanel.vue
+++ components/infopanels/CourseStatusInfoPanel.vue
@@ -3,7 +3,49 @@
     :title="$t('general.information._')"
     icon="mdi-information"
     :items="items"
-  />
+  >
+    <div v-if="$auth.user" class="text-center">
+      <!-- Publication button -->
+      <ApolloMutation
+        :mutation="require('../../gql/manage/publishCourse.gql')"
+        :variables="{ code: course.code }"
+        @done="published"
+      >
+        <template #default="{ mutate, loading }">
+          <v-btn
+            v-if="canPublish"
+            small
+            color="primary"
+            :loading="loading"
+            @click="mutate"
+          >
+            <v-icon left>mdi-cloud-upload</v-icon>
+            <span>{{ $t('course.publish') }}</span>
+          </v-btn>
+        </template>
+      </ApolloMutation>
+
+      <!-- Archive button -->
+      <ApolloMutation
+        :mutation="require('../../gql/manage/archiveCourse.gql')"
+        :variables="{ code: course.code }"
+        @done="archived"
+      >
+        <template #default="{ mutate, loading }">
+          <v-btn
+            v-if="canArchive"
+            small
+            color="primary"
+            :loading="loading"
+            @click="mutate"
+          >
+            <v-icon left>mdi-archive</v-icon>
+            <span>{{ $t('course.archive') }}</span>
+          </v-btn>
+        </template>
+      </ApolloMutation>
+    </div>
+  </generic-info-panel>
 </template>
 
 <script>
@@ -19,35 +61,40 @@ export default {
     },
   },
   computed: {
+    canArchive() {
+      return this.course.status === 'PUBLISHED'
+    },
+    canPublish() {
+      return this.course.status === 'UNPUBLISHED'
+    },
     items() {
       const items = []
 
-      // Course status
-      if (!this.course.isPublished) {
-        items.push({
-          icon: 'mdi-cloud-upload',
-          text: this.$t('course.status.unpublished'),
-          tooltip: this.$t('course.status._'),
-        })
-      } else if (!this.course.isArchived) {
-        items.push({
-          icon: 'mdi-cloud-upload',
-          text: this.$t('course.status.published_on', {
-            date: this.formatDateFull(this.course.published),
-          }),
-          tooltip: this.$t('course.status._'),
-        })
-      } else {
-        items.push({
-          icon: 'mdi-cloud-upload',
-          text: this.$t('course.status.archived_on', {
-            date: this.formatDateFull(this.course.archived),
-          }),
-          tooltip: this.$t('course.status._'),
-        })
+      // Course status.
+      const status = {
+        icon: 'mdi-cloud-upload',
+        tooltip: this.$t('course.status._'),
       }
+      switch (this.course.status) {
+        case 'UNPUBLISHED':
+          status.text = this.$t('course.status.unpublished')
+          break
 
-      // Course visibility
+        case 'PUBLISHED':
+          status.text = this.$t('course.status.published_on', {
+            date: this.formatDateFull(this.course.published),
+          })
+          break
+
+        case 'ARCHIVED':
+          status.text = this.$t('course.status.archived_on', {
+            date: this.formatDateFull(this.course.archived),
+          })
+          break
+      }
+      items.push(status)
+
+      // Course visibility.
       const visibility = this.course.visibility.toLowerCase()
       items.push({
         icon: 'mdi-eye',
@@ -56,6 +103,24 @@ export default {
       })
 
       return items
+    },
+  },
+  methods: {
+    archived({ data: { archiveCourse: course } }) {
+      this.$notificationManager.displaySuccessMessage(
+        this.$t('success.COURSE_ARCHIVED')
+      )
+      this.$router.push({
+        name: 'manage-courses-code',
+        params: { code: course.code },
+      })
+
+      // TODO: Fix the cache and redirect to the URL with the new course code
+    },
+    published() {
+      this.$notificationManager.displaySuccessMessage(
+        this.$t('success.COURSE_PUBLISHED')
+      )
     },
   },
 }
