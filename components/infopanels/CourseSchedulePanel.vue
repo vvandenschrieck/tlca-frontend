139ff1275cf3ef1dd631ccd47c3977ca08958cd3
Sébastien Combéfis <sebastien@combefis.be>
2022-08-13 16:03:05 +0200

    Set up skeleton loader for course schedule panel
    - Added a skeleton loader to display when the schedule is not available
    yet
    - Added a 'loading' prop to indicate that the data is being loaded

--- components/infopanels/CourseSchedulePanel.vue
+++ components/infopanels/CourseSchedulePanel.vue
@@ -1,19 +1,27 @@
 <template>
-  <generic-info-panel
-    v-if="schedule"
-    :title="$t('course.schedule._')"
-    icon="mdi-calendar"
-  >
-    <div class="mt-3 text-center">
-      <course-schedule dense :items="schedule" />
-    </div>
-  </generic-info-panel>
+  <div>
+    <generic-info-panel
+      v-if="schedule"
+      :title="$t('course.schedule._')"
+      icon="mdi-calendar"
+    >
+      <div class="mt-3 text-center">
+        <course-schedule dense :items="schedule" />
+      </div>
+    </generic-info-panel>
+
+    <v-skeleton-loader v-if="loading" type="image" />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'CourseSchedulePanel',
   props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
     schedule: {
       type: Array,
       default: null,
