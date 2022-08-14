7ec5e80c37bbbc18350930ec867c2eb8e38771fd
Sébastien Combéfis <sebastien@combefis.be>
2022-08-14 16:13:20 +0200

    Set up loading indicator to the generic info panel
    - Added a 'loading' prop to the generic info panel
    - Added an indeterminate linear progress bar to the card of the panel
    - Refactored the code of the generic info panel

--- components/infopanels/GenericInfoPanel.vue
+++ components/infopanels/GenericInfoPanel.vue
@@ -1,31 +1,30 @@
 <template>
-  <v-card>
+  <v-card :loading="loading">
     <v-card-title class="grey lighten-2 py-2">
-      <v-icon left size="20" v-text="icon"></v-icon>
-      <span class="subtitle-1" v-text="title"></span>
+      <v-icon left size="20">{{ icon }}</v-icon>
+      <span class="subtitle-1">{{ title }}</span>
     </v-card-title>
+
     <v-card-text class="px-0">
-      <v-list v-if="items && items.length" dense>
+      <v-list v-if="items?.length" dense>
         <template v-for="(item, i) in items">
           <v-list-item v-if="!item.details" :key="i" class="item mb-1">
             <v-list-item-icon class="ma-auto">
               <v-tooltip left open-delay="500">
                 <template #activator="{ on, attrs }">
-                  <v-icon
-                    v-bind="attrs"
-                    size="16"
-                    v-on="on"
-                    v-text="item.icon"
-                  ></v-icon>
+                  <v-icon size="16" v-bind="attrs" v-on="on">
+                    {{ item.icon }}
+                  </v-icon>
                 </template>
-                <span v-text="item.tooltip"></span>
+
+                <span>{{ item.tooltip }}</span>
               </v-tooltip>
             </v-list-item-icon>
+
             <v-list-item-content class="py-0">
-              <v-list-item-title
-                class="content"
-                v-text="item.text"
-              ></v-list-item-title>
+              <v-list-item-title class="content">
+                {{ item.text }}
+              </v-list-item-title>
             </v-list-item-content>
           </v-list-item>
 
@@ -34,33 +33,34 @@
               <v-list-item-icon class="ma-auto">
                 <v-tooltip left open-delay="500">
                   <template #activator="{ on, attrs }">
-                    <v-icon
-                      v-bind="attrs"
-                      size="16"
-                      v-on="on"
-                      v-text="item.icon"
-                    ></v-icon>
+                    <v-icon size="16" v-bind="attrs" v-on="on">
+                      {{ item.icon }}
+                    </v-icon>
                   </template>
-                  <span v-text="item.tooltip"></span>
+
+                  <span>{{ item.tooltip }}</span>
                 </v-tooltip>
               </v-list-item-icon>
+
               <v-list-item-content>
-                <v-list-item-title v-text="item.text"></v-list-item-title>
+                <v-list-item-title>{{ item.text }}</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
+
             <v-list-item
               v-for="(child, j) in item.details"
               :key="i + '-' + j"
               class="item mt-1 mb-2 pl-10"
             >
               <v-list-item-content class="py-0">
-                <v-list-item-title v-text="child"></v-list-item-title>
+                <v-list-item-title>{{ child }}</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
           </v-list-group>
         </template>
       </v-list>
-      <slot></slot>
+
+      <slot />
     </v-card-text>
   </v-card>
 </template>
@@ -69,10 +69,6 @@
 export default {
   name: 'GenericInfoPanel',
   props: {
-    title: {
-      type: String,
-      required: true,
-    },
     icon: {
       type: String,
       required: true,
@@ -81,15 +77,23 @@ export default {
       type: Array,
       default: undefined,
     },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
   },
 }
 </script>
 
 <style scoped>
-.item {
-  min-height: 10px !important;
-}
 .content {
   white-space: inherit;
+}
+.item {
+  min-height: 10px !important;
 }
 </style>
