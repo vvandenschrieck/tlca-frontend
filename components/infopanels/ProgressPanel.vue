4b81e68e1b124dae88c079dd3851d4bd7abfc3f5
Sébastien Combéfis <sebastien@combefis.be>
2022-08-14 14:48:21 +0200

    Initialise course detail page for learners
    - Created a progress panel component to display the progress
    - Created a generic component to show a progress
    - Refactored the learners list component to use the new progress component
    - Created a GraphQL query to retrieve a course for learn space

---
+++ components/infopanels/ProgressPanel.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <generic-info-panel
+      v-if="registration"
+      :title="$t('registration.progress')"
+      icon="mdi-progress-check"
+    >
+      <div class="d-flex justify-space-around mt-3">
+        <competencies-progress
+          :label="$t('competency.category.basic')"
+          :value="registration.progress.basic"
+        />
+        <competencies-progress
+          :label="$t('competency.category.advanced')"
+          :value="registration.progress.advanced"
+        />
+      </div>
+    </generic-info-panel>
+
+    <v-skeleton-loader v-if="loading" type="image" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProgressPanel',
+  props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    registration: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
--- pages/learn/courses/_code/index.vue
+++ pages/learn/courses/_code/index.vue
@@ -1,10 +1,54 @@
 <template>
-  <p>Learn : courses : _code</p>
+  <ApolloQuery
+    v-slot="{ result: { error, data: course }, isLoading }"
+    :query="require('~/gql/learn/getCourse.gql')"
+    :update="(data) => data.course"
+    :variables="{ code: $route.params.code }"
+    @result="setTitle"
+  >
+    <div v-if="!!isLoading">{{ $t('global.loading') }}</div>
+
+    <div v-else-if="course && course.isRegistered">
+      <h2>{{ title }}</h2>
+
+      <v-row>
+        <v-col cols="12" md="9">
+          {{ course }}
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="3"
+          :order="$vuetify.breakpoint.smAndDown ? 'first' : undefined"
+        >
+          <progress-panel class="mb-5" :registration="course.registration" />
+          <course-schedule-panel :schedule="course.schedule" />
+        </v-col>
+      </v-row>
+    </div>
+
+    <div v-else-if="error">{{ $t('error.unexpected') }}</div>
+  </ApolloQuery>
 </template>
 
 <script>
 export default {
   name: 'LearnCoursePage',
+  data() {
+    return {
+      title: '',
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+    }
+  },
+  methods: {
+    setTitle({ data: course }) {
+      this.title = course?.name || ''
+    },
+  },
   meta: {
     roles: ['student'],
   },
