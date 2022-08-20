14804d168b446aadd730c7a9b497a31632059268
Virginie Van den Schrieck <v.vandenschrieck@gmail.com>
2022-08-20 20:41:59 +0000

    Solving remaining bugs (dates and assessment code)

--- components/courses/AssessmentsTimeline.vue
+++ components/courses/AssessmentsTimeline.vue
@@ -30,62 +30,58 @@ export default {
   computed: {
     chartStyle() {
       let style = 'width: 100%; height: '
-      style = style + (this.assessments.length * 40 + 100) + 'px'
+      style = style + (this.assessments.length * 40 + 120) + 'px'
       return style
     },
     minDate() {
       let minDate = null
       if (this.schedule) {
         for (const event of this.schedule) {
-          if (event.name && event.name === 'start') {
-            minDate = event.datetime
-          }
+          if (event.name && event.name === 'start') minDate = event.datetime
         }
       }
-      for (const assessment of this.sortedAssessments) {
-        if (assessment.start) {
-          if (!minDate || assessment.start < minDate) minDate = assessment.start
-        }
-      }
+      if (minDate) return minDate
+
+      minDate = new Date().toISOString()
+      for (const assessment of this.sortedAssessments)
+        if (assessment.start && assessment.start < minDate)
+          minDate = assessment.start
       return minDate
     },
     maxDate() {
       let maxDate = null
       if (this.schedule) {
         for (const event of this.schedule) {
-          if (!maxDate && event.name && event.name === 'end') {
+          if (!maxDate && event.name && event.name === 'end')
             maxDate = event.datetime
-          }
-          if (event.name && event.name === 'evaluationsEnd') {
-            maxDate = event.datetime
-          }
+
+          if (event.name && event.name === 'evaluationsEnd')
+            return event.datetime
         }
+        if (maxDate) return maxDate
       }
-      for (const assessment of this.sortedAssessments) {
-        if (assessment.end) {
-          if (!maxDate || assessment.end > maxDate) maxDate = assessment.end
-        }
-      }
+      maxDate = new Date().toISOString()
+      for (const assessment of this.sortedAssessments)
+        if (assessment.end && assessment.end > maxDate) maxDate = assessment.end
       return maxDate
     },
     sortedAssessments() {
       return [...this.assessments].sort((a, b) => (a.start < b.start ? 1 : -1))
     },
     startdates() {
-      const dates = this.sortedAssessments.map((assessment) =>
+      return this.sortedAssessments.map((assessment) =>
         assessment.start ? assessment.start : this.minDate
       )
-      return dates
     },
     enddates() {
-      const dates = this.sortedAssessments.map((assessment) =>
+      return this.sortedAssessments.map((assessment) =>
         assessment.end ? assessment.end : this.maxDate
       )
-      return dates
     },
     assessementNames() {
-      const names = this.sortedAssessments.map((assessment) => assessment.code)
-      return names
+      return this.sortedAssessments.map((assessment) =>
+        this.shortName(assessment)
+      )
     },
     option() {
       return {
@@ -117,8 +113,9 @@ export default {
         xAxis: {
           type: 'time',
           position: 'top',
-          min: this.minDate,
-          max: this.maxDate,
+          // Prevents echarts to freeze if boundaries are not ordered
+          min: this.minDate < this.maxDate ? this.minDate : this.maxDate,
+          max: this.minDate > this.maxDate ? this.minDate : this.maxDate,
         },
         yAxis: {
           type: 'category',
@@ -151,6 +148,7 @@ export default {
       }
     },
   },
+
   methods: {
     goToAssessment(params) {
       if (params.componentType === 'yAxis' && params.value) {
@@ -165,6 +163,11 @@ export default {
           },
         })
       }
+    },
+    shortName(assessment) {
+      if (assessment.code) return assessment.code
+      if (assessment.name.length < 10) return assessment.name
+      return assessment.name.slice(0, 9)
     },
   },
 }
