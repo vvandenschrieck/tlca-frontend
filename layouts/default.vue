c55749583b3b961eee177fb09c01971ac8f00c74
Sébastien Combéfis <sebastien@combefis.be>
2022-09-15 17:34:06 +0200

    Update version number

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,7 +27,7 @@
 
     <v-footer app>
       <span class="text-caption">
-        {{ $t('global.powered_by') }} AEI Consulting – v1.0.4.
+        {{ $t('global.powered_by') }} AEI Consulting – v1.1.0.
       </span>
 
       <v-spacer />
