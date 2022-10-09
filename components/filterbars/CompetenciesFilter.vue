fae3e1f61fa55771d337212364b6529ae3d54eb1
Sébastien Combéfis <seb478@gmail.com>
2022-10-09 16:21:27 +0200

    Replace visibilities filter by a public one for competencies
    - Removed the competencies filter on the competencies list page in manage space
    - Added a filter to include public competencies
    - Excluded the public competencies by default on the competencies list page

--- components/filterbars/CompetenciesFilter.vue
+++ components/filterbars/CompetenciesFilter.vue
@@ -1,22 +1,20 @@
 <template>
   <div>
     <v-row>
-      <v-col cols="3">{{ $t('competency.filter.visibility') }}</v-col>
+      <v-col cols="3">{{ $t('competency.filter.public') }}</v-col>
       <v-col cols="9">
-        <v-select
-          clearable
+        <v-switch
+          class="mt-0 pt-0"
           dense
           hide-details
-          :items="visibilitiesList"
-          multiple
-          :value="value.visibilities"
-          @input="update('visibilities', $event)"
+          :value="value.includePublic"
+          @change="update('includePublic', $event)"
         />
       </v-col>
     </v-row>
 
     <v-row>
-      <v-col cols="3">{{ $t('competency.status.archived') }}</v-col>
+      <v-col cols="3">{{ $t('competency.filter.archived') }}</v-col>
       <v-col cols="9">
         <v-switch
           class="mt-0 pt-0"
@@ -73,31 +71,22 @@ export default {
         return {
           creators: null,
           includeArchived: false,
+          includePublic: false,
           tags: null,
-          visibilities: null,
         }
       },
     },
   },
   computed: {
     creatorsList() {
-      const visibilities = []
+      const creators = []
 
-      visibilities.push(
+      creators.push(
         { text: this.$t('competency.creator.own'), value: 'OWN' },
         { text: this.$t('competency.creator.partners'), value: 'PARTNERS' },
         { text: this.$t('competency.creator.external'), value: 'EXTERNAL' }
       )
-      return visibilities
-    },
-    visibilitiesList() {
-      const visibilities = []
-
-      visibilities.push(
-        { text: this.$t('competency.visibility.public'), value: 'PUBLIC' },
-        { text: this.$t('competency.visibility.private'), value: 'PRIVATE' }
-      )
-      return visibilities
+      return creators
     },
   },
   methods: {
