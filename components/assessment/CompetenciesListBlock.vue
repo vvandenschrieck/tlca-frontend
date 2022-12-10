a020fcacfcae48bef6b7ef3a376dded2ab2fbb54
Sébastien Combéfis <sebastien@combefis.be>
2022-12-10 08:34:52 +0100

    Show separately mandatory and optional competencies for assessments
    - Refactored the assessment competencies list component
    - Updated the pages displaying assessment competencies lists

---
+++ components/assessment/CompetenciesListBlock.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <template v-if="!!mandatory.length">
+      <h4>{{ $t('assessment.competencies.mandatory') }}</h4>
+
+      <v-list class="pa-0">
+        <template v-for="(competency, i) in mandatory">
+          <competency-list-item
+            :key="i * 2"
+            v-model="selectedCompetencies[i]"
+            :competency="competency"
+            :form="form"
+            :hide-checklist="hideChecklist"
+            :readonly="readonly"
+            :student-view="studentView"
+          />
+
+          <v-divider v-if="i < mandatory.length - 1" :key="i * 2 + 1" />
+        </template>
+      </v-list>
+    </template>
+
+    <template v-if="!!optional.length">
+      <h4>{{ $t('assessment.competencies.optional') }}</h4>
+
+      <v-list class="pa-0">
+        <template v-for="(competency, i) in optional">
+          <competency-list-item
+            :key="i * 2"
+            v-model="selectedCompetencies[mandatory.length + i]"
+            :competency="competency"
+            :form="form"
+            :hide-checklist="hideChecklist"
+            :readonly="readonly"
+            :student-view="studentView"
+          />
+
+          <v-divider v-if="i < optional.length - 1" :key="i * 2 + 1" />
+        </template>
+      </v-list>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AssessmentCompetenciesListBlock',
+  props: {
+    competencies: {
+      type: Array,
+      required: true,
+    },
+    form: {
+      type: Boolean,
+      default: false,
+    },
+    hideChecklist: {
+      type: Boolean,
+      default: false,
+    },
+    readonly: {
+      type: Boolean,
+      default: false,
+    },
+    studentView: {
+      type: Boolean,
+      default: false,
+    },
+    value: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    mandatory() {
+      return this.competencies.filter((c) => !c.isOptional)
+    },
+    optional() {
+      return this.competencies.filter((c) => c.isOptional)
+    },
+    selectedCompetencies: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        this.$emit('input', value)
+      },
+    },
+  },
+}
+</script>
