c5868f1258fd55066e942e4c560008ea2a2f961d
Sébastien Combéfis <sebastien@combefis.be>
2022-12-18 08:12:31 +0100

    Set up corrective release (v1.1.22)

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ export default {
       {
         apiKey: process.env.BUGSNAG_BROWSER_API_KEY,
         config: {
-          appVersion: '1.1.21',
+          appVersion: '1.1.22',
           enabledReleaseStages: ['production'],
           environment: process.env.NODE_ENV,
         },
