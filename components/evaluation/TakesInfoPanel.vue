2f96a596ed05dee401dee4fc6dce0b76520ad192
Sébastien Combéfis <sebastien@combefis.be>
2023-02-26 13:36:34 +0100

    Define a new assessment takes info panel
    - Defined a new info panel to display information about assessment takes
    - Defined a component to display the assessment takes status
    - Inserted the info panel on the assessment detail page (learn)

---
+++ components/evaluation/TakesInfoPanel.vue
@@ -0,0 +1,94 @@
+<template>
+  <ApolloQuery
+    v-slot="{ isLoading, result: { error } }"
+    :query="require('~/gql/infopanels/getEvaluationTakesInfo.gql')"
+    :update="(data) => data.assessment"
+    :variables="{ id: assessmentId }"
+    @result="setResult"
+  >
+    <generic-info-panel
+      icon="mdi-information"
+      :items="items"
+      :loading="!!isLoading"
+      :title="$tc('evaluation._', 2)"
+    >
+      <v-card-text v-if="!error && assessment">
+        <assessment-takes-status :assessment="assessment" />
+      </v-card-text>
+      <v-card-text v-if="error">{{ $t('error.unexpected') }}</v-card-text>
+    </generic-info-panel>
+  </ApolloQuery>
+</template>
+
+<script>
+import assessments from '@/mixins/assessments.js'
+import datetime from '@/mixins/datetime.js'
+
+export default {
+  name: 'EvaluationTakesInfoPanel',
+  mixins: [assessments, datetime],
+  props: {
+    assessmentId: {
+      type: String,
+      required: true,
+    },
+    hideAssessment: {
+      type: Boolean,
+      default: false,
+    },
+    hideLearner: {
+      type: Boolean,
+      default: false,
+    },
+    teacherView: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      assessment: null,
+    }
+  },
+  computed: {
+    items() {
+      if (!this.assessment) {
+        return []
+      }
+
+      const items = []
+
+      // Assessment instances counter.
+      const nbInstances = this.assessment.nbInstances
+      const maxInstances = this.assessment.instances ?? nbInstances + 1
+      const maxInstancesText =
+        this.assessment.instances ?? this.$t('general.infinity')
+      items.push({
+        icon: 'mdi-progress-check',
+        percentage: (nbInstances * 100) / maxInstances,
+        text: `${nbInstances} / ${maxInstancesText}`,
+        tooltip: this.$t('assessment.instances_status'),
+      })
+
+      // Assessment takes status.
+      const takesStatus = this.assessment.takesStatus.status
+      items.push({
+        icon: 'mdi-traffic-light',
+        text: takesStatus,
+        tooltip: this.$t('assessment.takes_status._'),
+      })
+
+      return items
+    },
+  },
+  methods: {
+    setResult({ data: assessment }) {
+      if (!assessment) {
+        return
+      }
+
+      this.assessment = assessment
+    },
+  },
+}
+</script>
