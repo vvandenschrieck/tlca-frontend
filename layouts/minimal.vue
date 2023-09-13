097fc6144191a346774e850ded39751dd1512314
Sébastien Combéfis <sebastien@combefis.be>
2023-09-13 08:28:19 +0200

    Set up corrective release (v1.2.4)

--- layouts/minimal.vue
+++ layouts/minimal.vue
@@ -13,7 +13,7 @@
 
     <v-footer app>
       <span class="text-caption">
-        {{ $t('global.powered_by') }} AEI Consulting – v1.2.3.
+        {{ $t('global.powered_by') }} AEI Consulting – v1.2.4.
       </span>
 
       <v-spacer />
